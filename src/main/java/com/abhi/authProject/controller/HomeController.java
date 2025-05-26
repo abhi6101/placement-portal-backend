981893090d5ab174cf345c60b47688e18912324e
abhi6101 <abhijain6101@gmail.com>
2025-05-26 16:42:44 +0530

    Removed @CrossOrigin from HomeController, relying on SecurityConfig

--- src/main/java/com/abhi/authProject/controller/HomeController.java
+++ src/main/java/com/abhi/authProject/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5500","http://127.0.0.1:5500"})
+
 public class HomeController {
 
     @GetMapping("/index")
