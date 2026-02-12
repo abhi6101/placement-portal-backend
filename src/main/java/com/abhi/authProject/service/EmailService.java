025b80afd4f837769d4aa6049024cdab0fd31a40
abhi6101 <abhijain6101@gmail.com>
2026-02-12 16:57:19 +0530

    Fix: Restore missing JSON imports in EmailService

--- src/main/java/com/abhi/authProject/service/EmailService.java
+++ src/main/java/com/abhi/authProject/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.abhi.authProject.service;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
